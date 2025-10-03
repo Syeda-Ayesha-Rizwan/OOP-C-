406f3c14dcbc3d10421178039b63889c6c39346e
Syeda-Ayesha-Rizwan <rizwanayesha588@gmail.com>
2025-10-03 23:50:19 +0500

    initial commit

---
+++ bank.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Bankaccount{
+    private:
+    int accountnumber;
+    float balance;
+    public:
+    void deposit(){
+
+    };
+    void withdraw(){
+
+    };
+};
