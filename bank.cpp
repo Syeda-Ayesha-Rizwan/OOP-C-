9177477d609ec7b7362b368814461a6a42845eb6
Syeda-Ayesha-Rizwan <rizwanayesha588@gmail.com>
2025-10-03 23:58:59 +0500

    BankAccount Class with Deposit and Withdraw

--- bank.cpp
+++ bank.cpp
@@ -1,14 +1,55 @@
 #include <iostream>
 using namespace std;
-class Bankaccount{
-    private:
-    int accountnumber;
-    float balance;
-    public:
-    void deposit(){
 
-    };
-    void withdraw(){
+class BankAccount {
+private:
+    int accountNumber;   
+    float balance;      
 
-    };
+public:
+
+    void setAccount(int accNo, float initialBalance) {
+        accountNumber = accNo;
+        balance = initialBalance;
+    }
+
+   
+    void deposit(float amount) {
+        if (amount > 0) {
+            balance = balance + amount;
+            cout << "Deposited: " << amount << endl;
+        } else {
+            cout << "Invalid deposit amount!" << endl;
+        }
+    }
+
+
+    void withdraw(float amount) {
+        if (amount > 0 && amount <= balance) {
+            balance = balance - amount;
+            cout << "Withdrawn: " << amount << endl;
+        } else {
+            cout << "Insufficient balance or invalid amount!" << endl;
+        }
+    }
+
+ 
+    void display() {
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Current Balance: " << balance << endl;
+    }
 };
+
+int main() {
+    BankAccount acc;
+
+    acc.setAccount(1234, 1000);  
+    acc.display();
+
+    acc.deposit(500);   
+    acc.withdraw(200);  
+    acc.withdraw(2000); 
+    acc.display();     
+
+    return 0;
+}
