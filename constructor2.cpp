cc18489ff8b8bf2b0afab2cca095fdaef19131d2
Syeda-Ayesha-Rizwan <rizwanayesha588@gmail.com>
2025-10-19 14:04:05 +0500

    RECTANGLE

---
+++ constructor2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Rectangle {
+    public:
+    int width ;
+    int height;
+    Rectangle(){
+        width = 0 ;
+        height = 0;
+    }
+    Rectangle(int width , int height ){
+        this -> width = width ;
+        this -> height = height ;
+
+    }
+    void display(){
+        cout << "width : " << width << endl;
+        cout << "height : " << height << endl;
+    }
+};
+    int main (){
+        Rectangle r1(22 , 33 );
+        Rectangle r2;
+        r1.display();
+        r2.display();
+    }
+
